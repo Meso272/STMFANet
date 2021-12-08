9db8d9ce0dafce679aa34a7dfc007e5c35dfa73c
Meso <liujinyang1201@126.com>
2021-12-07 17:07:43 -0800

    123

--- wavenet_models/STMF_Model.py
+++ wavenet_models/STMF_Model.py
@@ -82,9 +82,9 @@ class STMFModel(BaseModel):
             target_batch=targets[:,:,:,:,i]
             pred_batch=the_pred[i]
             for j in range(targets.shape[0]):
-                print(target_batch[j])
-                print(pred_batch[j])
-                pr+=util.psnr(target_batch[j],pred_batch[j])
+                #print(target_batch[j])
+                #print(pred_batch[j])
+                pr+=util.psnr(target_batch[j].cpu(),pred_batch[j].cpu())
                 count+=1
         if keep_state:
             self.state=old_state
